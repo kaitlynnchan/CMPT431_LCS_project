7c7ce7343a0e16c45f8e1cf9d73d419423204bd8
Shiva <shiva.26622e@gmail.com>
2024-11-18 21:54:38 -0800

    adding serial code

---
+++ README.md
@@ -0,0 +1,8 @@
+
+## Usage
+
+Instructions for running the project.
+
+```bash
+# Example command to run the project
+./lcs_serial input_files/text_one.txt input_files/text_two.txt
--- lcs_serial.cpp
+++ lcs_serial.cpp
@@ -1,4 +1,30 @@
 #include "core/read_file.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Returns length of LCS for s1[0..m-1], s2[0..n-1]
+int lcs(string &s1, string &s2) {
+    int m = s1.size();
+    int n = s2.size();
+
+    // Initializing a matrix of size (m+1)*(n+1)
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    // Building dp[m+1][n+1] in bottom-up fashion
+    for (int i = 1; i <= m; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            if (s1[i - 1] == s2[j - 1])
+                dp[i][j] = dp[i - 1][j - 1] + 1;
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    // dp[m][n] contains length of LCS for s1[0..m-1]
+    // and s2[0..n-1]
+    return dp[m][n];
+}
 
 int main(int argc, char *argv[]) {
     
@@ -6,6 +32,8 @@ int main(int argc, char *argv[]) {
     string string2 = readFile(argv[2]);
     std::cout << "String 1 : " << string1 << "\n";
     std::cout << "String 2 : " << string2 << "\n";
+
+    cout << lcs(string1, string2) << endl;
 
     return 0; 
 }
